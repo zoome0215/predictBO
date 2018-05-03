b54895b6f94d5126c134a2bbbabfea98e4d7b28e
Tomohiro <t.hashizume.brisbane@gmail.com>
2018-05-03 10:26:45 +0100

    Added sample backtest result in beforeandafter.txt

--- tensorflow/calcwinrate.py
+++ tensorflow/calcwinrate.py
@@ -1,12 +1,13 @@
 from __future__ import division
 initmoney=200
-asset=789.0
+asset=2170
 bet=20
 rate=0.85
-NTA=150
+NTA=598
 
 x = (-asset + initmoney + bet*rate*NTA)/(bet*(rate+1))
 rate = 1-x/NTA
 
+print 'Number of wins are', NTA-x
 print 'winning rate is',rate,'%'
 
